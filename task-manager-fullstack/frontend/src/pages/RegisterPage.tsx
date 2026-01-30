43caae7c5f5835e23eb5ad487743c0152d627be3
Tomasposada26 <tomasposada26@gmail.com>
2026-01-30 14:51:07 -0500

    Add authentication pages and routing structure to frontend

---
+++ task-manager-fullstack/frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Register</h2>
+      {/* TODO: Add register form */}
+    </div>
+  );
+};
+
+export default RegisterPage;
